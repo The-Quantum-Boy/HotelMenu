61d08cdff8b261b53ef3c3d49249a836fe030329
The-Quantum-Boy <dhanorkar.sumit.code@gmail.com>
2022-09-12 14:57:14 +0530

    react app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menuItems,setMenuItems]=useState(items);
+  const [categories,setCategories]=useState([]);
+  
+  const filterItems=(category)=>{
+    if(category==='all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems=items.filter((item)=>item.category===category);
+    setMenuItems(newItems);
+  }
+  return <main>
+    <section className='menu section'>
+        <div className="title">
+          <h2>Hotel Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems}/>
+        <Menu items={menuItems}/>
+    </section>
+  </main>;
+}
+
+export default App;
---
+++ src/Categories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Categories = ({filterItems}) => {
+
+  return <div className='btn-container'>
+      <button className='filter-btn' onClick={() => filterItems('all')}>
+         all
+      </button>
+      <button className='filter-btn' onClick={()=>filterItems('breakfast')}>
+          breakfast
+      </button>
+
+    <button className='filter-btn' onClick={() => filterItems('breakfast')}>
+      breakfast
+    </button>
+  </div>;
+};
+
+export default Categories;
