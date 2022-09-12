aa88a789e0bf849c6fe69dddb71016d2d79fb0f0
The-Quantum-Boy <dhanorkar.sumit.code@gmail.com>
2022-09-12 15:09:38 +0530

    final chages

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories =['all',... new Set(items.map((item)=>item.category))];
+console.log(allCategories);
 function App() {
   const [menuItems,setMenuItems]=useState(items);
-  const [categories,setCategories]=useState([]);
+  const [categories,setCategories]=useState(allCategories);
   
   const filterItems=(category)=>{
     if(category==='all'){
@@ -21,7 +23,7 @@ function App() {
           <h2>Hotel Menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems}/>
+        <Categories categories={categories} filterItems={filterItems}/>
         <Menu items={menuItems}/>
     </section>
   </main>;
